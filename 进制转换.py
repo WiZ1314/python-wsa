7fec4d526a6795873637a6f24ee777d324b2ef91
王申奥 <w13625408378@163.com>
2022-03-14 09:18:07 +0800

    3.14

---
+++ 进制转换.py
@@ -0,0 +1,9 @@
+# coding:utf-8
+# 工程文件:python-wsa
+# 代码文件:进制转换
+# 创建时间:2022/3/14 8:51
+r_number = int(input('输入一个数'))
+print('十进制为：', r_number)
+print('转换二进制为：', bin(r_number))
+print('转换八进制为：', oct(r_number))
+print('转换十六进制为：', hex(r_number))
